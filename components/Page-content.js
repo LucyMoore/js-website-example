59ba4f592b0d34f3d8450a67c044d338b6317754
LucyMoore <lucyviolet.moore@gmail.com>
2016-06-03 17:34:39 +1200

    all buttons in place

--- components/Page-content.js
+++ components/Page-content.js
@@ -8,13 +8,16 @@ import Menu from './Menu'
 module.exports = class Content extends React.Component {
 	constructor(props){
     	super(props)
-    	this.state={content: 'home'}
+    	this.state={content: ''}
 	}
 
 	update(e){
 		var toDisplay = e.target.className
 		if(toDisplay === 'home'){
-			console.log('test')
+			this.setState({content: 'home'})
+		}
+		else if(toDisplay === 'map'){
+			this.setState({content: 'map'})
 		}
 	}
 
@@ -24,13 +27,33 @@ module.exports = class Content extends React.Component {
 		    	<div id="menu">
 			    	<Button className="home" txt="Home" onClick={this.update.bind(this)} />
 			    	<Button className="map" txt="Map" onClick={this.update.bind(this)}  />
-			    	<Button className="18th" txt="18th Dynasty" />
-			    	<Button className="19th" txt="19th Dynasty" />
-			    	<Button className="20th" txt="20th Dynasty" />
-			    	<Button className="21st" txt="21st Dynasty" />
+			    	<Button className="18th" txt="18th Dynasty" onClick={this.update.bind(this)}/>
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+
+			    	<Button className="19th" txt="19th Dynasty" onClick={this.update.bind(this)}/>
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+
+			    	<Button className="20th" txt="20th Dynasty" onClick={this.update.bind(this)}/>
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+
+			    	<Button className="21st" txt="21st Dynasty" onClick={this.update.bind(this)}/>
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+			    	<Button className="hidden" txt="Home" onClick={this.update.bind(this)} />
+
 		    	</div>
 		    	<div id="content">
-		    	{this.state.content}
+		    		{this.state.content}
 	    		</div>
 	    	</div>
 	   	)
